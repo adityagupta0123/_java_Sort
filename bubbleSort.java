869053dca029bb8e2f977eac986377b66623fe81
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2021-12-10 18:31:45 +0530

    change code as userInput.

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,11 +1,14 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Bubblesort {
     public static void main(String[]args){
-
-        int a[]={1,-2,-33,7,31,12};
-        int n=a.length;
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the length of array");
+        int n = sc.nextInt();
+        int a[]= new int [n];
+     
         for (int i=0; i<n-1; i++ ){
             for(int j=0; j<n-1-i; j++){
 
